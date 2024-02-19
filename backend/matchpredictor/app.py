36899571706b82dc3995523ef5aa120ffbc6060c
gbangban <yao.gban@gmail.com>
2024-02-19 11:15:58 -0500

    IMR: Step 1 completion.  Still needs App.py cleanup

--- backend/matchpredictor/app.py
+++ backend/matchpredictor/app.py
@@ -14,6 +14,7 @@ from matchpredictor.predictors.home_predictor import HomePredictor
 from matchpredictor.predictors.linear_regression_predictor import train_regression_predictor
 from matchpredictor.predictors.past_results_predictor import train_results_predictor
 from matchpredictor.predictors.simulation_predictor import train_offense_and_defense_predictor, train_offense_predictor
+from matchpredictor.predictors.alphabet_predictor import AlphabetPredictor
 from matchpredictor.teams.teams_api import teams_api
 from matchpredictor.teams.teams_provider import TeamsProvider
 from matchpredictor.upcominggames.football_data_api_client import FootballDataApiClient
@@ -21,13 +22,15 @@ from matchpredictor.upcominggames.upcoming_games_api import upcoming_games_api
 
 
 def build_model_provider(training_data: List[Result]) -> ModelProvider:
+    print("===========  Slower models are commented out here ===============")
     return ModelProvider([
         Model("Home", HomePredictor()),
         Model("Points", train_results_predictor(training_data)),
-        Model("Offense simulator (fast)", train_offense_predictor(training_data, 1_000)),
-        Model("Offense simulator", train_offense_predictor(training_data, 10_000)),
-        Model("Full simulator (fast)", train_offense_and_defense_predictor(training_data, 1_000)),
-        Model("Full simulator", train_offense_and_defense_predictor(training_data, 10_000)),
+        Model("==Alphabet City==", AlphabetPredictor()),
+        # Model("Offense simulator (fast)", train_offense_predictor(training_data, 1_000)),
+        # Model("Offense simulator", train_offense_predictor(training_data, 10_000)),
+        # Model("Full simulator (fast)", train_offense_and_defense_predictor(training_data, 1_000)),
+        # Model("Full simulator", train_offense_and_defense_predictor(training_data, 10_000)),
         # The linear regression model uses scikit learn, so can cause issues on some machines
         # Model("Linear regression", train_regression_predictor(training_data))
     ])
--- backend/matchpredictor/predictors/alphabet_predictor.py
+++ backend/matchpredictor/predictors/alphabet_predictor.py
@@ -3,7 +3,7 @@ from matchpredictor.predictors.predictor import Prediction, Predictor
 
 
 class AlphabetPredictor(Predictor):
-    def predict(self, fixture: Fixture) -> Prediction:
-        home_name, away_name = Fixture.home_team.name, Fixture.away_team.name
+    def predict(self, fixture: Fixture) -> Prediction:        
+        home_name, away_name = fixture.home_team.name, fixture.away_team.name
         outcome = Outcome.HOME if home_name < away_name else Outcome.AWAY
         return Prediction(outcome)
